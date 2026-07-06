706978e15ef502ea3e0d287ea916bd98047a2b25
Iroqu0is <duovit82@gmail.com>
2026-07-06 18:46:29 +0300

    Delete _Итоговая работа   4  месяца directory
    Delete _Итоговая работа   4  месяца directory

--- _Итоговая работа   4  месяца/ConsoleApp1/IUseless.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleApp1
-{
-    public interface IUseless<T> where T : struct, IComparable<T>, INumber<T>
-    {
-        string Name { get; }
-        string Surname { get; }
-        T Age { get; }
-    }
-}
