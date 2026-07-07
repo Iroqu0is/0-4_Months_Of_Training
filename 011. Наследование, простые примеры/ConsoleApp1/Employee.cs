c04a65ff467df9a09fff357d1768e936cea4abad
Iroqu0is <duovit82@gmail.com>
2026-07-07 12:36:44 +0300

    Add files via upload

---
+++ 011. Наследование, простые примеры/ConsoleApp1/Employee.cs
@@ -0,0 +1,105 @@
+﻿namespace ConsoleApp1
+{
+    using ResE = (Employee? Instance, bool IsDone, string? Message);
+    public enum Gender : byte { Undecided, Male, Female }
+    public class Employee : Person, IEquatable<Employee>, IComparable<Employee>
+    {
+        protected byte exp;
+        protected Gender gen;
+
+        public byte Experience { get { return exp; } }
+        public Gender Gender { get { return gen; } }
+
+        protected Employee(Person person, byte experience, Gender gender) : base(person)
+        {
+            exp = experience;
+            gen = gender;
+        }
+        protected Employee(string name, string surname, byte age, byte experience, Gender gender) : base(name, surname, age)
+        {
+            exp = experience;
+            gen = gender;
+        }
+        public static ResE Build(string? name, string? surname, byte? age, byte experience, Gender gender = Gender.Undecided)
+        {
+            var tmp = Person.Build(name, surname, age);
+            if (!tmp.IsDone) return (null, false, tmp.Message);
+            if (experience > 50) return (null, false, "Invalid experience.");
+            return (new Employee(tmp.Instance!, experience, gender), true, "Instance created.");
+        }
+
+        private static bool Equals(Employee? e1, Employee? e2)
+        {
+            return Person.Equals(e1, e2) && (e1!.exp == e2!.exp) && (e1!.gen == e2!.gen);
+        }
+        private static int CompareTo(Employee? e1, Employee? e2)
+        {
+            var res = Person.CompareTo(e1, e2);
+            if (res != 0) return res;
+            res = e1!.exp.CompareTo(e2!.exp);
+            if (res != 0) return res;
+            return e1!.gen.CompareTo(e2!.gen);
+        }
+
+        public int CompareTo(Employee? employee)
+        {
+            return Employee.CompareTo(this, employee);
+        }
+        public bool Equals(Employee? employee)
+        {
+            return Employee.Equals(this, employee);
+        }
+        public override bool Equals(object? obj)
+        {
+            var employee = obj as Employee;
+            return Employee.Equals(this, employee);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), exp, (byte)gen);
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()} Experience: {exp}, gender: {gen}.";
+        }
+
+        public static bool operator ==(Employee? e1, Employee? e2)
+        {
+            return Employee.Equals(e1, e2);
+        }
+        public static bool operator !=(Employee? e1, Employee? e2)
+        {
+            return !Employee.Equals(e1, e2);
+        }
+
+        public static bool operator <(Employee? e1, Employee? e2)
+        {
+            return Employee.CompareTo(e1, e2) < 0;
+        }
+        public static bool operator >(Employee? e1, Employee? e2)
+        {
+            return Employee.CompareTo(e1, e2) > 0;
+        }
+        public static bool operator <=(Employee? e1, Employee? e2)
+        {
+            return Employee.CompareTo(e1, e2) <= 0;
+        }
+        public static bool operator >=(Employee? e1, Employee? e2)
+        {
+            return Employee.CompareTo(e1, e2) >= 0;
+        }
+
+        public static bool operator true(Employee? employee)
+        {
+            return employee is not null;
+        }
+        public static bool operator false(Employee? employee)
+        {
+            return employee is null;
+        }
+        public static bool operator !(Employee? employee)
+        {
+            return !(employee is not null);
+        }
+    }
+}
---
+++ 011. Наследование, простые примеры/ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    public class MyException : Exception
+    {
+        private readonly object? _value;
+        public object? Value { get { return _value; } }
+        public MyException(object? obj = null) : base() { _value = obj; }
+        public MyException(string? str, object? obj = null) : base(str) { _value = obj; }
+        public MyException(string? str, Exception? inner, object? obj = null) : base(str, inner) { _value = obj; }
+
+        public override string ToString()
+        {
+            if (_value is null) return Message;
+            return $"Main error message: {Message}\nAdditional data: {_value}.";
+        }
+    }
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            var methodName = MethodBase.GetCurrentMethod()?.Name;
+            Console.WriteLine($"Method '{methodName ?? "Main"}' started.\n");
+            var timer = Stopwatch.StartNew();
+            try
+            {
+                var persons = new Person[]
+                {
+                    Person.Build("Tom","Kukuruz",55).Instance!,
+                    Person.Build("Silvester","Sostolovoy",66).Instance!,
+                    Person.Build("Tom","Bambadil",99).Instance!,
+                    Person.Build("Anton","Baton",29).Instance!,
+                    Person.Build("Alice","Selezneva",11).Instance!,
+                    Person.Build("Arnold","Shwartznegger",23).Instance!,
+                    Person.Build("Rumpel","Stiltskin",91).Instance!,
+                    Person.Build("Klara","Ukrala",16).Instance!,
+                    Person.Build("Carl","Koralov",13).Instance!,
+                    Person.Build("Buba","Smith",45).Instance!,
+                    Person.Build("Anton","Chehov",41).Instance!
+                };
+
+
+                var select = persons.Where(arg => arg is not null)
+                                    .Distinct()
+                                    .OrderBy(arg => arg)
+                                    .GroupBy(arg => arg.Name);
+
+                foreach (var tmp in select)
+                {
+                    Console.WriteLine(tmp.Key);
+                    foreach (var item in tmp)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine();
+                }
+                throw new MyException("Test !!!!", "One more message (если вдруг одного было мало)");
+            }
+            catch (MyException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine($"\nMethod '{methodName ?? "Main"}' stopped in {timer.ElapsedMilliseconds}ms.");
+                GC.Collect();
+            }
+            return 0;
+        }
+    }
+}
