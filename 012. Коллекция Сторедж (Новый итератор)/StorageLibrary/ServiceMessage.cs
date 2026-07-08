6f37a0eb9a6aba2791bed5030c44cd2fe85edd19
Iroqu0is <duovit82@gmail.com>
2026-07-08 09:42:57 +0300

    Новый итератор

---
+++ 012. Коллекция Сторедж (Новый итератор)/StorageLibrary/ServiceMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace StorageLibrary
+{
+    public class ServiceMessage : EventArgs
+    {
+        private readonly string _message;
+        public string Message { get { return _message; } }
+
+        public ServiceMessage(string? message = null)
+        {
+            _message = message ?? "No message.";
+        }
+    }
+}
