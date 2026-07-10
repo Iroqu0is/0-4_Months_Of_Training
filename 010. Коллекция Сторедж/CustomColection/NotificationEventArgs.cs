6b80940290c4ea2649687eba44d2266e7b280c9d
Iroqu0is <duovit82@gmail.com>
2026-07-10 16:54:09 +0300

    Add files via upload

---
+++ 010. Коллекция Сторедж/CustomColection/NotificationEventArgs.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomColection
+{
+    public sealed class NotificationEventArgs : EventArgs
+    {
+        private readonly string _message;
+        public string Message { get { return _message; } }
+        public NotificationEventArgs(string? message = null)
+        {
+            _message = message ?? "N/A";
+        }
+        public override string ToString()
+        {
+            return _message;
+        }
+    }
+}
