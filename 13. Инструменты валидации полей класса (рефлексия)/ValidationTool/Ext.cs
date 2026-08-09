fe424ac8b8be6e480e8b319254d13dbd691807ef
Iroqu0is <duovit82@gmail.com>
2026-08-09 15:27:03 +0300

    Add files via upload

---
+++ 13. Инструменты валидации полей класса (рефлексия)/ValidationTool/Ext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace ValidationTool
+{
+    public static class Ext
+    {
+        public static bool InRange(this object? obj)
+        {
+            if (obj is null) throw new ArgumentNullException(nameof(obj));
+            var type = obj.GetType();
+            var fields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            if (fields.Length == 0) return true;
+            foreach (var field in fields)
+            {
+                var attr = field.GetCustomAttribute<Range>();
+                if (attr is null) continue;
+                var tmp = field.GetValue(obj);
+                if (tmp is IComparable value)
+                {
+                    if (value.CompareTo(attr.LeftBound) < 0 || value.CompareTo(attr.RightBound) > 0) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ 13. Инструменты валидации полей класса (рефлексия)/ValidationTool/Range.cs
@@ -0,0 +1,42 @@
+﻿namespace ValidationTool
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class Range : Attribute
+    {
+        private readonly IComparable lb;
+        private readonly IComparable rb;
+
+        public IComparable LeftBound { get { return lb; } }
+        public IComparable RightBound { get { return rb; } }
+
+        public Range(object? leftbound, object? rightbound)
+        {
+            if (leftbound is null) throw new ArgumentNullException($"{nameof(leftbound)} is null");
+            if (rightbound is null) throw new ArgumentNullException($"{nameof(rightbound)} is null");
+
+            var _lb = leftbound as IComparable;
+            if (_lb is null || !_lb.GetType().IsValueType) throw new ArgumentException($"{nameof(leftbound)} не является числом");
+
+            var _rb = rightbound as IComparable;
+            if (_rb is null || !_rb.GetType().IsValueType) throw new ArgumentException($"{nameof(rightbound)} не является числом");
+
+            if (_lb.GetType().Name != _rb.GetType().Name) throw new ArgumentException("Параметры должны быть одного типа");
+            if (_lb.CompareTo(_rb) >= 0) throw new ArgumentException("Неправильно задан диапазон");
+
+            lb = _lb;
+            rb = _rb;
+        }
+        public static bool operator true(Range? attr)
+        {
+            return attr is not null;
+        }
+        public static bool operator false(Range? attr)
+        {
+            return attr is null;
+        }
+        public static bool operator !(Range? attr)
+        {
+            return attr is null;
+        }
+    }
+}
