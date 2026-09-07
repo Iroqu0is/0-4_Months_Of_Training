5323cce4c7a2abb7cd68abeb261ecbae637cab1c
Iroqu0is <duovit82@gmail.com>
2026-09-07 20:10:01 +0300

    Add files via upload

---
+++ 15. Просто упражнения/ConsoleApp1/NotificationMessage.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp1
+{
+    public class NotificationMessage : EventArgs
+    {
+        private static int counter;
+        static NotificationMessage()
+        {
+            counter = 0;
+        }
+
+        private readonly int id;
+        private readonly string _message;
+
+        public int RecordNumber { get { return id; } }
+        public string Message { get { return _message; } }
+        public int MessagesCreated { get { return counter; } }
+
+        public NotificationMessage(string? message = null)
+        {
+            id = Interlocked.Increment(ref counter);
+            _message = message ?? "Notification not recived";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return ReferenceEquals(this, obj);
+        }
+        public override int GetHashCode()
+        {
+            return id;
+        }
+        public override string ToString()
+        {
+            return _message;
+        }
+
+        public static bool operator ==(NotificationMessage nf1, NotificationMessage nf2)
+        {
+            return ReferenceEquals(nf1, nf2);
+        }
+        public static bool operator !=(NotificationMessage nf1, NotificationMessage nf2)
+        {
+            return !ReferenceEquals(nf1, nf2);
+        }
+    }
+}
---
+++ 15. Просто упражнения/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿global using System.Collections;
+global using System.Diagnostics;
+global using System.Reflection;
+global using System.Text;
+global using static System.Math;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            var cc = new ConcurrentCollection<int>();
+            cc.Add(9);
+            cc.Add(1);
+            cc.Add(2);
+            cc.Add(3);
+            cc.Add(4);
+            cc.Add(5);
+            cc.Add(6);
+            cc.Remove(9);
+            cc.Trim();
+
+            foreach (int i in cc)
+            {
+                Console.WriteLine(i);
+            }
+
+            return 0;
+        }
+    }
+}
